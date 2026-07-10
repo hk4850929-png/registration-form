d43179d507456b3f139da92866abf8a4b646534d
hk4850929-png <hk4850929@gmail.com>
2026-07-10 14:20:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", function(e){
-    e.preventDefault();
+form.addEventListener("submit", function(event){
+    event.preventDefault();
 
     alert("Registration Successful!");
 });
